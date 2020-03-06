492047c2432457a06211aae5ce60013ae6726728
alfredoav2 <alfredo.almazan@saei.mx>
2020-03-06 08:58:48 -0600

    con extra

---
+++ Tarea3_unity/Assets/Script/LevelLoade.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoade : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1) ;
+    }
+}
---
+++ Tarea3_unity/Assets/Script/TerrainGenerator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour{
+    public int depth = 20;
+    public int width = 256;
+    public int height = 256;
+    public float scale = 20f;
+    public float offsetx = 100f;
+    public float offsety = 100f;
+    private void Start(){
+        offsetx = Random.Range(0f, 9999f);
+        offsety = Random.Range(0f, 9999f);
+    }
+    private void Update() {
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+        offsetx += Time.deltaTime * 5f;
+    }
+    TerrainData GenerateTerrain(TerrainData terrainData){
+        terrainData.heightmapResolution = width + 1;
+        terrainData.size = new Vector3(width, depth, height);
+        terrainData.SetHeights(0, 0, GenerateHeights());
+        return terrainData;
+    }
+    float[,] GenerateHeights(){
+        float[,] heights = new float[width, height];
+        for(int x = 0; x < width; x++){
+            for(int y = 0; y < height; y++){
+                heights[x, y] = CalculateHeight(x, y);
+            }
+        }
+        return heights;
+    }
+    float CalculateHeight(int x, int y){
+        float xCoord = (float)x / width  * scale + offsetx;
+        float yCoord = (float)y / height * scale + offsety;
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+}
